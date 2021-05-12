a7285ee026dc445efbf3b4ccef355ddaec30f9d8
Restyled.io <commits@restyled.io>
2021-05-12 17:23:22 +0000

    Restyled by prettier

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,13 @@
 // GLOBAL VARIABLES
 
 const myLibrary = [];
-const submitButton = document.querySelector('.submit-btn');
-const form = document.querySelector('form');
-const addBook = document.querySelector('.add-book');
+const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector("form");
+const addBook = document.querySelector(".add-book");
 
 // FUNCTIONS
 
-function Book(title, author, pages, read = 'Not yet read') {
+function Book(title, author, pages, read = "Not yet read") {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -20,13 +20,14 @@ function addBookToLibrary(title, author, pages) {
 }
 
 function readStatus() {
-  this.parentNode.read = this.parentNode.read === 'Not yet read'
-                             ? 'Finished reading'
-                             : 'Not yet read';
-  this.parentNode.querySelector('.book-read').textContent =
-      this.parentNode.querySelector('.book-read').textContent === 'Not yet read'
-          ? 'Finished reading'
-          : 'Not yet read';
+  this.parentNode.read =
+    this.parentNode.read === "Not yet read"
+      ? "Finished reading"
+      : "Not yet read";
+  this.parentNode.querySelector(".book-read").textContent =
+    this.parentNode.querySelector(".book-read").textContent === "Not yet read"
+      ? "Finished reading"
+      : "Not yet read";
 }
 
 function deleteBook() {
@@ -35,50 +36,50 @@ function deleteBook() {
 }
 
 function pushToDom() {
-  const container = document.querySelector('.js-container');
-  container.innerHTML = '';
+  const container = document.querySelector(".js-container");
+  container.innerHTML = "";
 
   myLibrary.forEach((book) => {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
 
-    const title = document.createElement('h3');
-    title.classList.add('book-title');
+    const title = document.createElement("h3");
+    title.classList.add("book-title");
     title.textContent = book.title;
 
-    const author = document.createElement('p');
-    author.classList.add('book-author');
+    const author = document.createElement("p");
+    author.classList.add("book-author");
     author.textContent = `Author: ${book.author}`;
 
-    const pages = document.createElement('p');
-    pages.classList.add('book-pages');
+    const pages = document.createElement("p");
+    pages.classList.add("book-pages");
     pages.textContent = `Pages: ${book.pages}`;
 
-    const read = document.createElement('button');
-    read.classList.add('book-read');
+    const read = document.createElement("button");
+    read.classList.add("book-read");
     read.textContent =
-        book.read === 'Not yet read' ? 'Not yet read' : 'Finished reading.';
+      book.read === "Not yet read" ? "Not yet read" : "Finished reading.";
 
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete-button');
-    deleteButton.textContent = 'Delete';
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.textContent = "Delete";
 
     bookDiv.append(title, author, pages, read, deleteButton);
     container.appendChild(bookDiv);
 
-    read.addEventListener('click', readStatus);
+    read.addEventListener("click", readStatus);
 
-    deleteButton.addEventListener('click', deleteBook);
+    deleteButton.addEventListener("click", deleteBook);
   });
 }
 
 // EVENT LISTENERS
 
-submitButton.addEventListener('click', (bookData) => {
+submitButton.addEventListener("click", (bookData) => {
   bookData.preventDefault();
-  const title = document.getElementById('title').value;
-  const author = document.getElementById('author').value;
-  let pages = document.getElementById('pages').value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
   // eslint-disable-next-line radix
   pages = parseInt(pages);
   addBookToLibrary(title, author, pages);
@@ -86,12 +87,12 @@ submitButton.addEventListener('click', (bookData) => {
   pushToDom();
 });
 
-addBook.addEventListener('click', (e) => {
-  if (form.classList.contains('d-none')) {
-    e.target.textContent = 'Hide Form';
-    form.classList.remove('d-none');
+addBook.addEventListener("click", (e) => {
+  if (form.classList.contains("d-none")) {
+    e.target.textContent = "Hide Form";
+    form.classList.remove("d-none");
   } else {
-    e.target.textContent = 'Add Book';
-    form.classList.add('d-none');
+    e.target.textContent = "Add Book";
+    form.classList.add("d-none");
   }
 });
